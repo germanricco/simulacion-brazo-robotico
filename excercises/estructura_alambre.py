41fea49fe032434c5bb081b5e3891c324351b0ce
GermanRicco <germanricco97@gmail.com>
2025-01-23 16:52:08 -0300

    Agegado calculo de angulos de euler

--- excercises/estructura_alambre.py
+++ excercises/estructura_alambre.py
@@ -13,8 +13,6 @@ print(f"Project Root: {project_root}")
 # Agregar la ruta relativa al sys.path
 cinematica_path = project_root / "src" / "cinematica"
 sys.path.append(str(cinematica_path))
-print(f"Ruta Agregada:", cinematica_path)
-print("Contenido de la carpeta:", list(cinematica_path.glob("*.py")))
 
 # Intentar importar el módulo
 try:
@@ -23,27 +21,48 @@ try:
 except ModuleNotFoundError:
     print("Error: No se pudo importar el modulo 'cinematica' ")
 
+# Matriz de parametros geométricos y posición inicial
 matriz_dh_params = np.array([
-        [0, np.pi/2, 1, np.radians(0)],  # a, alpha, d, theta
-        [1, 0, 0, np.radians(0)],
-        [1, 0, 0, np.radians(0)]
+        [1, 0, 0, np.radians(0)],        #GCS to MCS
+        [0, np.pi/2, 0.5, np.radians(0)],  #MCS to J1
+        [1, 0, 0, np.radians(0)],        #J1 to J2
+        [0.8, 0, 0, np.radians(0)],        #J2 to J3
+        [0.4, 0, 0, np.radians(0)]     #J3 to TCP (wrist to end of tool)
     ])
 
 # Función de actualización de la visualización con sliders
 def actualizar(val):
-    matriz_dh_params[0, 3] = np.radians(slider_theta1.val)  # Actualiza theta1
-    matriz_dh_params[1, 3] = np.radians(slider_theta2.val)  # Actualiza theta2
-    matriz_dh_params[2, 3] = np.radians(slider_theta3.val)
+    # Actualizar ángulos theta con slider (4 gdl)
+    matriz_dh_params[1, 3] = np.radians(slider_theta1.val)
+    matriz_dh_params[2, 3] = np.radians(slider_theta2.val)
+    matriz_dh_params[3, 3] = np.radians(slider_theta3.val)
+    matriz_dh_params[4, 3] = np.radians(slider_theta4.val)
 
-    x, y, z = cinematica.calcular_posiciones(matriz_dh_params)
+    # Obtener posicion y orientacion
+    posiciones, R_tcp  = cinematica.cinematica_directa(matriz_dh_params)
+
+    # Convertir orientacion en angulos de euler
+    angulos_euler= cinematica.calcular_angulos_euler(R_tcp)
+
+    #Conviertir angulos de euler de rad2deg
+    angulos_euler_deg = []
+    for angulo in angulos_euler:
+        angulos_euler_deg.append(np.degrees(angulo))
 
     ax.cla()  # Limpiar el gráfico
-    ax.plot(x, y, z, 'bo-', linewidth=2, label="Brazo robótico")  # Dibujar el brazo
-    ax.scatter(x[-1], y[-1], z[-1], color='red', s=100, label="TCP")  # TCP en rojo
+    ax.scatter(posiciones[0,0], posiciones[0,1], posiciones[0,2], color='green', s=100, label="GCS")     # GCS en verde
+    ax.plot(posiciones[1:,0], posiciones[1:,1], posiciones[1:,2], 'bo-', linewidth=2, label="Brazo robótico")  # Dibujar el brazo
+    ax.scatter(posiciones[-1,0], posiciones[-1,1], posiciones[-1,2], color='red', s=100, label="TCP")       # TCP en rojo
 
-    # Ajustar límites de los ejes
-    ax.set_xlim([-3, 3])
-    ax.set_ylim([-3, 3])
+    # Actualizo textos
+    ax.text2D(0.05, 0.95, f"Posición TCP: X={posiciones[-1,0]:.2f}, Y={posiciones[-1,1]:.2f}, Z={posiciones[-1,2]:.2f}",
+              transform=ax.transAxes, fontsize=12, color='black')
+    ax.text2D(0.05, 0.90, f"Ángulos de Euler: A={angulos_euler_deg[0]:.2f}, B={angulos_euler_deg[1]:.2f}, C={angulos_euler_deg[2]:.2f}",
+              transform=ax.transAxes, fontsize=10, color='black')
+
+    # Ajustar límites de los ejes y etiquetas
+    ax.set_xlim([-2, 4])
+    ax.set_ylim([-2, 4])
     ax.set_zlim([0, 5])
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
@@ -53,18 +72,22 @@ def actualizar(val):
     plt.draw()
 
 # Configuración de la figura 3D
-fig = plt.figure(figsize=(8, 8))
+fig = plt.figure(figsize=(8, 6))
 ax = fig.add_subplot(111, projection='3d')
 
-# Visualización inicial
-x, y, z = cinematica.calcular_posiciones(matriz_dh_params)
-ax.plot(x, y, z, 'bo-', linewidth=2)
-ax.scatter(x[-1], y[-1], z[-1], color='red', s=100)
-
 # Ajustar límites iniciales de los ejes
-ax.set_xlim([-3, 3])
-ax.set_ylim([-3, 3])
+ax.set_xlim([-2, 4])
+ax.set_ylim([-2, 4])
 ax.set_zlim([-0, 5])
+
+# Visualización inicial
+posiciones, R_tcp = cinematica.cinematica_directa(matriz_dh_params)
+
+ax.scatter(posiciones[0,0], posiciones[0,1], posiciones[0,2], color='green', s=100) 
+ax.plot(posiciones[1:,0], posiciones[1:,1], posiciones[1:,2], 'bo-', linewidth=2)
+ax.scatter(posiciones[-1,0], posiciones[-1,1], posiciones[-1,2], color='red', s=100)
+
+# Agregar Etiquetas y Titulo
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
 ax.set_zlabel("Z")
@@ -75,15 +98,18 @@ axcolor = 'lightgoldenrodyellow'
 ax_theta1 = plt.axes([0.2, 0.02, 0.65, 0.03], facecolor=axcolor)
 ax_theta2 = plt.axes([0.2, 0.06, 0.65, 0.03], facecolor=axcolor)
 ax_theta3 = plt.axes([0.2, 0.1, 0.65, 0.03], facecolor=axcolor)
+ax_theta4 = plt.axes([0.2, 0.14, 0.65, 0.03], facecolor=axcolor)
 
 slider_theta1 = Slider(ax_theta1, 'Theta 1', -180, 180, valinit=0, valstep=1)
 slider_theta2 = Slider(ax_theta2, 'Theta 2', -180, 180, valinit=0, valstep=1)
 slider_theta3 = Slider(ax_theta3, 'Theta 3', -180, 180, valinit=0, valstep=1)
+slider_theta4 = Slider(ax_theta4, 'Theta 4', -180, 180, valinit=0, valstep=1)
 
 # Asignar la función de actualización a los sliders
 slider_theta1.on_changed(actualizar)
 slider_theta2.on_changed(actualizar)
 slider_theta3.on_changed(actualizar)
+slider_theta4.on_changed(actualizar)
 
 # Mostrar la visualización interactiva
 plt.show()
--- src/cinematica/cinematica.py
+++ src/cinematica/cinematica.py
@@ -1,17 +1,11 @@
 import numpy as np
-
 import sys
 from pathlib import Path
 
 # Obtener el directorio raíz del proyecto
 project_root = Path(__file__).resolve().parent.parent.parent
-print(f"Project Root: {project_root}")
-
-# Agregar la ruta relativa al sys.path
 algebra_path = project_root / "src" / "algebra_lineal"
 sys.path.append(str(algebra_path))
-#print(f"Ruta Agregada:", algebra_path)
-#print("Contenido de la carpeta:", list(algebra_path.glob("*.py")))
 
 # Intentar importar el módulo
 try:
@@ -20,12 +14,15 @@ try:
 except ModuleNotFoundError:
     print("Error: No se pudo importar el modulo 'transformaciones'")
 
-# Función principal para calcular las posiciones de los puntos a partir de la matriz DH
-def calcular_posiciones(matriz_dh_params):
+def cinematica_directa(matriz_dh_params):
     """
     Calcula las posiciones de las articulaciones utilizando parámetros DH.
+
     Parámetros:
         matriz_dh_params: Matriz numpy de tamaño (n, 4) con los parámetros DH (a, alpha, d, theta)
+
+        siendo n la cantidad de articulaciones o puntos de interes
+
     Retorna:
         Matriz numpy con las posiciones x, y, z de las articulaciones y la matriz de orientación final
     """
@@ -38,20 +35,37 @@ def calcular_posiciones(matriz_dh_params):
     # Recorrer los parámetros DH y calcular la posición de cada articulación
     for fila in matriz_dh_params:
         a, alpha, d, theta = fila
-        T_i = transformaciones.matriz_dh(a, alpha, d, theta)  # Obtener matriz homogénea para la articulación actual
-        T = np.dot(T, T_i)  # Acumulación de la transformación
+        T_i = transformaciones.matriz_dh(a, alpha, d, theta)
+        T = np.dot(T, T_i)
 
         # Agregar la posición del extremo (x, y, z, 1) para mantener la dimensión consistente
         nueva_posicion = np.append(T[:3, 3], 1)
-        # Stackeo matriz en forma vertical, agregando la nueva posición
+        # Agrego a matriz la nueva posición
         puntos_homogeneos = np.vstack((puntos_homogeneos, nueva_posicion))
 
     # Obtengo la matriz de rotación de la ultima matriz homogenea (Orientación final del TCP)
-    orientacion_tcp = T[:3, :3]
+    R_tcp = T[:3, :3]
 
-    # Transponer la matriz de puntos para devolver las coordenadas en columnas [x, y, z]
-    return puntos_homogeneos[:, :3].T, orientacion_tcp # Devuelve las posiciones y la matriz de orientación final
+    return puntos_homogeneos[:, :3], R_tcp
 
+def calcular_angulos_euler(R):
+    """
+    Calcula los ángulos de euler (A, B, C) a partir de una matriz de rotación 3x3
+
+    Parámetros:
+        R (numpy.ndarray): Matriz de rotación 3x3
+
+    Retorna:
+        list: [A, B, C] en radianes
+    """
+
+    B = np.arctan2(-R[2, 0], np.sqrt(1 - R[2, 0]**2))
+    A = np.arctan2(R[2, 1], R[2, 2])
+    C = np.arctan2(R[1, 0], R[0, 0])
+
+    return [A, B, C]
+
+#
 
 if __name__ == "__main__":
     # Prueba con una matriz DH de ejemplo (2 articulaciones)
@@ -61,8 +75,17 @@ if __name__ == "__main__":
         [1, 0, 0, np.radians(0)]
     ])
 
-    posiciones, orientacion_tcp = calcular_posiciones(matriz_dh_params)
-    print("Coordenadas: X en fila 0, Y en fila 1, Z en fila 2")
+    posiciones, orientacion_tcp = cinematica_directa(matriz_dh_params)
+    print("Cada fila representa la posicion de una articulacion")
     print(posiciones)
     print("Matriz de orientacion final: ")
     print(orientacion_tcp)
+    angulos_euler = calcular_angulos_euler(orientacion_tcp)
+    print(f"Los angulos de Euler (en radianes) son: {angulos_euler}")
+    
+    #Convierto angulos de euler de rad2deg
+    angulos_euler_deg = []
+    for angulo in angulos_euler:
+        angulos_euler_deg.append(np.degrees(angulo))
+
+    print(f"Los angulos de Euler (en grados) son: {angulos_euler_deg}")
